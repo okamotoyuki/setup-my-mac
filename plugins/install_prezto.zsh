08d27cdab2e01c392833006cb0b76ad61190db2f
OkamotoYuki <okamoto.yuki.0130@gmail.com>
2020-08-16 16:30:15 +0900

    update plugins/install_prezto.zsh not to install prezto if it has already been installed

--- plugins/install_prezto.zsh
+++ plugins/install_prezto.zsh
@@ -1,11 +1,16 @@
 #!/usr/bin/env zsh
 
-# Install prezto
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+# Check if prezto has alraedy been installed
+ls "${ZDOTDIR:-$HOME}/.zprezto" > /dev/null 2>&1
 
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
+# Install prezto if it hasn't been installed yet
+if [ $? -ne 0 ]; then
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  done
+fi
 
 # Check if git module has alraedy been installed
 grep "'git'" $HOME/.zprezto/runcoms/zpreztorc > /dev/null 2>&1
