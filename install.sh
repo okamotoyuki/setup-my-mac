edb7cf446beb69363f37c21d34b4a9a2fd9fb690
OkamotoYuki <okamoto.yuki.0130@gmail.com>
2020-08-16 23:43:05 +0900

    remove unnecessary Brewfile copy

--- install.sh
+++ install.sh
@@ -3,9 +3,6 @@
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-# Copy Browfile to home directory
-cp ./Brewfile $HOME/
-
 # Install Homebrew bundles
 brew bundle
 
